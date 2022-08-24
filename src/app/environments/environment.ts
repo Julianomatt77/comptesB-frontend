e4b46a088bb5f8974eea1608caeaf91e9e3a8044
Julien MARTIN <ju.martin@outlook.com>
2022-08-24 11:17:09 +0200

    fix deployment bugs

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,13 +2,10 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  // appVersion: 'versionSEMWEE',
-  // USERDATA_KEY: 'authf649fc9a5f55',
-  // isMockEnabled: false,
-  baseUrl: 'http://localhost:3000',
-};
+// export const environment = {
+//   production: true,
+//   baseUrl: 'https://jm-comptesb.herokuapp.com/api',
+// };
 
 /*
  * For easier debugging in development mode, you can import the following file
