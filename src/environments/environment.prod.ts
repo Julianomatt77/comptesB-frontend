c1dfa7801c46f0c1e3ae5c3d591d7c3b79098d83
Julien MARTIN <ju.martin@outlook.com>
2022-10-24 09:46:27 +0200

    fix fly.io deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://jm-comptesb.herokuapp.com/api',
+  // baseUrl: 'https://jm-comptesb.herokuapp.com/api',
   // baseUrl: 'https://compty-api.onrender.com/api',
   // baseUrl: 'https://compty.fly.dev/api',
+  baseUrl: 'https://compty2.fly.dev/api'
 };
