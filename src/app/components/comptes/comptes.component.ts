9e0b0cbddd7f8be279969fde54175fe7d4ed5665
Julien MARTIN <ju.martin@outlook.com>
2024-02-19 10:09:27 +0100

    update export typo

--- src/app/components/comptes/comptes.component.ts
+++ src/app/components/comptes/comptes.component.ts
@@ -705,7 +705,7 @@ export class ComptesComponent implements OnInit, OnDestroy {
         arrayToExport.push({
           Date: data.history[0].dateSolde,
           Compte: data.name,
-          'Solde Initial': data.history[0].soldeInitia,
+          'Solde Initial': data.history[0].soldeInitial,
           Différence: data.history[0].montant,
           'Solde Final': data.history[0].soldeFinal,
         });
